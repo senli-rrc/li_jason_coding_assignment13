4fc457886d9fbd9130efe5c58479b5cfc48aa48d
Jason Li <senli@rrc.ca>
2025-07-20 18:22:02 -0500

    testing pre commit checks

--- li_jason_ui_garden_build_checks/src/components/Button/Button.tsx
+++ li_jason_ui_garden_build_checks/src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ import { ButtonProps } from "./Button.types";
 interface StyledButtonProps {
   $backgroundColor?: string;
   $disabled?: boolean;
-  // test1
+  // test2
 }
 
 const StyledButton = styled.button<StyledButtonProps>`
